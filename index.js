61fed5285d9c4d8eb43e31f24cabfc40181e202b
Vivian Gomez <viviangomez0@gmail.com>
2021-11-24 23:40:11 -0500

    commit tree 55b9f4ab05408c69b7cc7539fb10d1e48c4fadc7

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(books=>renderBooks(books))
 }
+function initialize(){
+  fetchBooks()
+}
+initialize()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
